4f914d1bd05c51f82c4c23650e132ee15d08a4bc
MagzhanArtykov <magzhan_artykov@epam.com>
2023-12-20 18:22:48 +0600

    MODREQMED-2 Create module skeleton - SpringBoot

--- src/test/java/org/folio/mr/RequestMediatorApplicationTest.java
+++ src/test/java/org/folio/mr/RequestMediatorApplicationTest.java
@@ -10,16 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.folio.spring.liquibase.FolioLiquibaseConfiguration;
 import org.folio.tenant.domain.dto.TenantAttributes;
 import org.folio.tenant.rest.resource.TenantApi;
 
-@ActiveProfiles({"test", "testcontainers-pg"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class FolioSpringTemplateApplicationTest {
+class RequestMediatorApplicationTest {
 
   @EnableAutoConfiguration(exclude = {FolioLiquibaseConfiguration.class})
   @RestController("folioTenantController")
