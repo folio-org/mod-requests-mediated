c66bc3537a5997a76da8ccba9476ec17fe95eed1
alexanderkurash <alexander.kurash@gmail.com>
2024-08-05 18:26:59 +0300

    MODREQMED-10 Add more identifier tests

--- src/test/java/org/folio/mr/domain/entity/MediatedRequestInstanceIdentifierTest.java
+++ src/test/java/org/folio/mr/domain/entity/MediatedRequestInstanceIdentifierTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class MediatedRequestInstanceIdentifierTest {
   @ParameterizedTest
   @CsvSource(value = {
+    // Non-null values
     "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, true",
     "bb97a683-d710-4c73-9010-a85c5160e299, 61e01a2a-da55-4d52-a286-b58e9b92436c, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, false",
     "bb97a683-d710-4c73-9010-a85c5160e299, 61e01a2a-da55-4d52-a286-b58e9b92436c, 70a334e2-e295-4105-aa77-56fc731f147d, c87e8af7-a4e5-4b9c-8c40-d0e12d03dff9, valueA, valueA, false",
@@ -17,24 +18,94 @@ class MediatedRequestInstanceIdentifierTest {
     "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, c87e8af7-a4e5-4b9c-8c40-d0e12d03dff9, valueA, valueA, false",
     "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, c87e8af7-a4e5-4b9c-8c40-d0e12d03dff9, valueA, valueB, false",
     "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueB, false",
+    // Null values
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, true",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, null, valueA, valueA, true",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, null, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, null, true",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, valueA, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, valueA, null, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, null, valueA, false",
+    "bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, null, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, null, valueA, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, null, valueA, null, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, valueA, false",
+    "null, bb97a683-d710-4c73-9010-a85c5160e299, null, null, null, null, false",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, true",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, true",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, valueA, false",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, null, false",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, valueA, false",
+    "null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, null, false",
+    "null, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, valueA, false",
+    "null, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, valueA, null, false",
+    "null, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, valueA, false",
+    "null, null, null, 70a334e2-e295-4105-aa77-56fc731f147d, null, null, false",
+    "null, null, null, null, valueA, valueA, true",
+    "null, null, null, null, valueA, null, false",
+    "null, null, null, null, null, valueA, false",
+    "null, null, null, null, null, null, true",
   })
   void testEqualsMethod(String mediatedRequestIdA, String mediatedRequestIdB,
     String identifierTypeIdA, String identifierTypeIdB, String valueA, String valueB, boolean equals) {
 
     var mediatedRequestA = new MediatedRequestEntity();
-    mediatedRequestA.setId(UUID.fromString(mediatedRequestIdA));
+    mediatedRequestA.setId(uuidFromString(mediatedRequestIdA));
     var identifierA = new MediatedRequestInstanceIdentifier();
     identifierA.setMediatedRequest(mediatedRequestA);
-    identifierA.setIdentifierTypeId(UUID.fromString(identifierTypeIdA));
+    identifierA.setIdentifierTypeId(uuidFromString(identifierTypeIdA));
     identifierA.setValue(valueA);
 
     var mediatedRequestB = new MediatedRequestEntity();
-    mediatedRequestB.setId(UUID.fromString(mediatedRequestIdB));
+    mediatedRequestB.setId(uuidFromString(mediatedRequestIdB));
     var identifierB = new MediatedRequestInstanceIdentifier();
     identifierB.setMediatedRequest(mediatedRequestB);
-    identifierB.setIdentifierTypeId(UUID.fromString(identifierTypeIdB));
+    identifierB.setIdentifierTypeId(uuidFromString(identifierTypeIdB));
     identifierB.setValue(valueB);
 
     assertEquals(equals, identifierA.equals(identifierB));
+  }
+
+  private UUID uuidFromString(String uuidString) {
+    return uuidString == null || "null".equals(uuidString)
+      ? null
+      : UUID.fromString(uuidString);
   }
 }
