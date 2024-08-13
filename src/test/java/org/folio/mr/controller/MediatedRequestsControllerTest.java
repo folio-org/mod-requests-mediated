f01f7fcfe52eade5dc1a038cb74f736a4a500c51
Irakli Merabishvili <irakli_merabishvili1@epam.com>
2024-08-13 14:39:38 +0400

    MODREQMED-14: Create skeleton for a new endpoint - confirm item arrival

--- src/test/java/org/folio/mr/controller/MediatedRequestsControllerTest.java
+++ src/test/java/org/folio/mr/controller/MediatedRequestsControllerTest.java
@@ -1,5 +1,6 @@
 package org.folio.mr.controller;
 
+import org.folio.mr.domain.dto.ConfirmItemArrivalRequest;
 import org.folio.mr.domain.dto.MediatedRequest;
 import org.folio.mr.service.MediatedRequestsService;
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatusCode;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -28,13 +30,19 @@ class MediatedRequestsControllerTest {
     when(requestsService.get(any())).thenReturn(Optional.empty());
     var response = requestsController.getMediatedRequestById(any());
     verify(requestsService).get(any());
-    Assertions.assertEquals(response.getStatusCode(), HttpStatusCode.valueOf(404));
+    Assertions.assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
   }
 
   @Test
   void getById() {
     when(requestsService.get(any())).thenReturn(Optional.of(new MediatedRequest()));
     var response = requestsController.getMediatedRequestById(any());
-    Assertions.assertEquals(response.getStatusCode(), HttpStatusCode.valueOf(200));
+    Assertions.assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+  }
+
+  @Test
+  void confirmItemArrival() {
+    var response = requestsController.confirmItemArrival("", mock(ConfirmItemArrivalRequest.class));
+    Assertions.assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
   }
 }
