7275fb5c03b322059818a25b8abbc63125bb4034
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-08-13 14:16:21 +0300

    MODREQMED-11 Extract MediatedRequestDetailsService

---
+++ src/main/java/org/folio/mr/service/impl/MediatedRequestDetailsServiceImpl.java
@@ -0,0 +1,252 @@
+package org.folio.mr.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.folio.mr.client.InstanceClient;
+import org.folio.mr.client.ItemClient;
+import org.folio.mr.client.LocationClient;
+import org.folio.mr.client.LocationUnitClient;
+import org.folio.mr.client.ServicePointClient;
+import org.folio.mr.client.UserClient;
+import org.folio.mr.client.UserGroupClient;
+import org.folio.mr.domain.dto.Instance;
+import org.folio.mr.domain.dto.InstanceContributorsInner;
+import org.folio.mr.domain.dto.Item;
+import org.folio.mr.domain.dto.Library;
+import org.folio.mr.domain.dto.Location;
+import org.folio.mr.domain.dto.MediatedRequest;
+import org.folio.mr.domain.dto.MediatedRequestInstanceContributorNamesInner;
+import org.folio.mr.domain.dto.MediatedRequestInstancePublicationInner;
+import org.folio.mr.domain.dto.MediatedRequestItemCallNumberComponents;
+import org.folio.mr.domain.dto.MediatedRequestItemLocation;
+import org.folio.mr.domain.dto.MediatedRequestPickupServicePoint;
+import org.folio.mr.domain.dto.MediatedRequestProxyPatronGroup;
+import org.folio.mr.domain.dto.MediatedRequestRequesterPatronGroup;
+import org.folio.mr.domain.dto.ServicePoint;
+import org.folio.mr.domain.dto.User;
+import org.folio.mr.domain.dto.UserGroup;
+import org.folio.mr.domain.dto.UserPersonal;
+import org.folio.mr.service.MediatedRequestDetailsService;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class MediatedRequestDetailsServiceImpl implements MediatedRequestDetailsService {
+  private final ItemClient itemClient;
+  private final InstanceClient instanceClient;
+  private final ServicePointClient servicePointClient;
+  private final UserClient userClient;
+  private final UserGroupClient userGroupClient;
+  private final LocationClient locationClient;
+  private final LocationUnitClient locationUnitClient;
+
+  public MediatedRequest fetchRequestDetails(MediatedRequest request) {
+    fetchItem(request);
+    fetchRequester(request);
+    fetchProxyUser(request);
+    fetchInstance(request);
+    fetchPickupServicePoint(request);
+
+    //    request.getMetadata()
+    //      .createdByUserId(UUID.randomUUID().toString())
+    //      .updatedByUserId(UUID.randomUUID().toString());
+
+    return request;
+  }
+
+  private void fetchItem(MediatedRequest request) {
+    final String itemId = request.getItemId();
+    if (itemId == null) {
+      log.info("fetchItem:: itemId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchItem:: fetching item {}", itemId);
+    Item item = itemClient.get(itemId);
+
+    request.getItem()
+      .enumeration(item.getEnumeration())
+      .volume(item.getVolume())
+      .chronology(item.getChronology())
+      .displaySummary(item.getDisplaySummary())
+      .status(item.getStatus().getName().getValue())
+      .copyNumber(item.getCopyNumber());
+
+    var effectiveCallNumberComponents = item.getEffectiveCallNumberComponents();
+    if (effectiveCallNumberComponents != null) {
+      request.getItem()
+        .callNumber(effectiveCallNumberComponents.getCallNumber())
+        .callNumberComponents(new MediatedRequestItemCallNumberComponents()
+          .callNumber(effectiveCallNumberComponents.getCallNumber())
+          .prefix(effectiveCallNumberComponents.getPrefix())
+          .suffix(effectiveCallNumberComponents.getSuffix()));
+    }
+
+    fetchLocation(request, item.getEffectiveLocationId());
+  }
+
+  private void fetchLocation(MediatedRequest request, String effectiveLocationId) {
+    if (effectiveLocationId == null) {
+      log.info("fetchLocation:: effectiveLocationId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchLocation:: fetching location {}", effectiveLocationId);
+    Location location = locationClient.get(effectiveLocationId);
+
+    request.getItem()
+      .location(new MediatedRequestItemLocation()
+        .name(location.getName())
+        .code(location.getCode()));
+
+    fetchLibrary(request, location.getLibraryId());
+  }
+
+  private void fetchLibrary(MediatedRequest request, String libraryId) {
+    if (libraryId == null) {
+      log.info("fetchLibrary:: libraryId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchLibrary:: fetching library {}", libraryId);
+    Library library = locationUnitClient.getLibrary(libraryId);
+    request.getItem().getLocation().setLibraryName(library.getName());
+  }
+
+  private void fetchRequester(MediatedRequest request) {
+    final String requesterId = request.getRequesterId();
+    if (requesterId == null) {
+      log.info("fetchRequester:: requesterId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchRequester:: fetching requester {}", requesterId);
+    User requester = userClient.get(request.getRequesterId());
+
+    request.getRequester()
+      .barcode(requester.getBarcode())
+      .patronGroupId(requester.getPatronGroup());
+
+    UserPersonal personal = requester.getPersonal();
+    if (personal != null) {
+      request.getRequester()
+        .firstName(personal.getFirstName())
+        .middleName(personal.getMiddleName())
+        .lastName(personal.getLastName());
+    }
+
+    fetchRequesterUserGroup(request, requester.getPatronGroup());
+  }
+
+  private void fetchRequesterUserGroup(MediatedRequest request, String userGroupId) {
+    if (userGroupId == null) {
+      log.info("fetchRequesterUserGroup:: userGroupId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchRequesterUserGroup:: fetching requester user group {}", userGroupId);
+    UserGroup userGroup = userGroupClient.get(userGroupId);
+
+    request.getRequester()
+      .patronGroup(new MediatedRequestRequesterPatronGroup()
+        .id(userGroup.getId())
+        .group(userGroup.getGroup())
+        .desc(userGroup.getDesc()));
+  }
+
+  private void fetchProxyUser(MediatedRequest request) {
+    final String proxyUserId = request.getProxyUserId();
+    if (proxyUserId == null) {
+      log.info("fetchProxyUser:: proxyUserId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchProxyUser:: fetching proxy user {}", proxyUserId);
+    User proxyUser = userClient.get(proxyUserId);
+
+    request.getProxy()
+      .barcode(proxyUser.getBarcode())
+      .patronGroupId(proxyUser.getPatronGroup());
+
+    UserPersonal personal = proxyUser.getPersonal();
+    if (personal != null) {
+      request.getProxy()
+        .firstName(personal.getFirstName())
+        .middleName(personal.getMiddleName())
+        .lastName(personal.getLastName());
+    }
+
+    fetchProxyUserGroup(request, proxyUser.getPatronGroup());
+  }
+
+  private void fetchProxyUserGroup(MediatedRequest request, String userGroupId) {
+    if (userGroupId == null) {
+      log.info("fetchProxyUserGroup:: userGroupId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchProxyUserGroup:: fetching proxy user group {}", userGroupId);
+    UserGroup userGroup = userGroupClient.get(userGroupId);
+
+    request.getProxy()
+      .patronGroup(new MediatedRequestProxyPatronGroup()
+        .id(userGroup.getId())
+        .group(userGroup.getGroup())
+        .desc(userGroup.getDesc()));
+  }
+
+  private void fetchInstance(MediatedRequest request) {
+    final String instanceId = request.getInstanceId();
+    if (instanceId == null) {
+      log.info("fetchInstance:: instanceId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchInstance:: fetching instance {}", instanceId);
+    Instance instance = instanceClient.get(request.getInstanceId());
+
+    List<MediatedRequestInstanceContributorNamesInner> contributors = instance.getContributors()
+      .stream()
+      .map(InstanceContributorsInner::getName)
+      .map(new MediatedRequestInstanceContributorNamesInner()::name)
+      .toList();
+
+    List<MediatedRequestInstancePublicationInner> publications = instance.getPublication()
+      .stream()
+      .map(publication -> new MediatedRequestInstancePublicationInner()
+        .publisher(publication.getPublisher())
+        .place(publication.getPlace())
+        .dateOfPublication(publication.getDateOfPublication())
+        .role(publication.getRole()))
+      .toList();
+
+    request.getInstance()
+      .contributorNames(contributors)
+      .publication(publications)
+      .editions(new ArrayList<>(instance.getEditions()));
+  }
+
+  private void fetchPickupServicePoint(MediatedRequest request) {
+    final String pickupServicePointId = request.getPickupServicePointId();
+    if (pickupServicePointId == null) {
+      log.info("fetchPickupServicePoint:: pickupServicePointId is null, doing nothing");
+      return;
+    }
+
+    log.info("fetchPickupServicePoint:: fetching service point {}", pickupServicePointId);
+    ServicePoint pickupServicePoint = servicePointClient.get(request.getPickupServicePointId());
+
+    request.pickupServicePoint(new MediatedRequestPickupServicePoint()
+      .name(pickupServicePoint.getName())
+      .code(pickupServicePoint.getCode())
+      .discoveryDisplayName(pickupServicePoint.getDiscoveryDisplayName())
+      .description(pickupServicePoint.getDescription())
+      .shelvingLagTime(pickupServicePoint.getShelvingLagTime())
+      .pickupLocation(pickupServicePoint.getPickupLocation()));
+  }
+}
--- src/main/java/org/folio/mr/service/impl/MediatedRequestsServiceImpl.java
+++ src/main/java/org/folio/mr/service/impl/MediatedRequestsServiceImpl.java
@@ -2,38 +2,15 @@ package org.folio.mr.service.impl;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.folio.mr.client.HoldingsRecordClient;
-import org.folio.mr.client.InstanceClient;
-import org.folio.mr.client.ItemClient;
-import org.folio.mr.client.LocationClient;
-import org.folio.mr.client.LocationUnitClient;
-import org.folio.mr.client.ServicePointClient;
-import org.folio.mr.client.UserClient;
-import org.folio.mr.client.UserGroupClient;
-import org.folio.mr.domain.dto.Instance;
-import org.folio.mr.domain.dto.Item;
-import org.folio.mr.domain.dto.Library;
-import org.folio.mr.domain.dto.Location;
 import org.folio.mr.domain.dto.MediatedRequest;
-import org.folio.mr.domain.dto.MediatedRequestInstanceContributorNamesInner;
-import org.folio.mr.domain.dto.MediatedRequestInstancePublicationInner;
-import org.folio.mr.domain.dto.MediatedRequestItemCallNumberComponents;
-import org.folio.mr.domain.dto.MediatedRequestItemLocation;
-import org.folio.mr.domain.dto.MediatedRequestPickupServicePoint;
-import org.folio.mr.domain.dto.MediatedRequestProxyPatronGroup;
-import org.folio.mr.domain.dto.MediatedRequestRequesterPatronGroup;
 import org.folio.mr.domain.dto.MediatedRequests;
-import org.folio.mr.domain.dto.ServicePoint;
-import org.folio.mr.domain.dto.User;
-import org.folio.mr.domain.dto.UserGroup;
 import org.folio.mr.domain.entity.MediatedRequestEntity;
 import org.folio.mr.domain.mapper.MediatedRequestMapper;
 import org.folio.mr.repository.MediatedRequestsRepository;
+import org.folio.mr.service.MediatedRequestDetailsService;
 import org.folio.mr.service.MediatedRequestsService;
 import org.folio.spring.data.OffsetRequest;
 import org.springframework.stereotype.Service;
@@ -45,22 +22,16 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @Log4j2
 public class MediatedRequestsServiceImpl implements MediatedRequestsService {
-  private final ItemClient itemClient;
-  private final InstanceClient instanceClient;
-  private final ServicePointClient servicePointClient;
-  private final UserClient userClient;
-  private final UserGroupClient userGroupClient;
-  private final LocationClient locationClient;
-  private final LocationUnitClient locationUnitClient;
 
   private final MediatedRequestsRepository mediatedRequestsRepository;
   private final MediatedRequestMapper requestsMapper;
+  private final MediatedRequestDetailsService requestDetailsService;
 
   @Override
   public Optional<MediatedRequest> get(UUID id) {
     return mediatedRequestsRepository.findById(id)
       .map(requestsMapper::mapEntityToDto)
-      .map(this::extendMediatedRequestDto);
+      .map(requestDetailsService::fetchRequestDetails);
   }
 
   @Override
@@ -68,7 +39,7 @@ public class MediatedRequestsServiceImpl implements MediatedRequestsService {
     var mediatedRequests =
       mediatedRequestsRepository.findByCql(query, OffsetRequest.of(offset, limit)).stream()
         .map(requestsMapper::mapEntityToDto)
-        .map(this::extendMediatedRequestDto)
+        .map(requestDetailsService::fetchRequestDetails)
         .toList();
 
     var totalRecords = mediatedRequestsRepository.count(query);
@@ -81,7 +52,7 @@ public class MediatedRequestsServiceImpl implements MediatedRequestsService {
     var mediatedRequests = mediatedRequestsRepository.findAll(OffsetRequest.of(offset, limit))
       .stream()
       .map(requestsMapper::mapEntityToDto)
-      .map(this::extendMediatedRequestDto)
+      .map(requestDetailsService::fetchRequestDetails)
       .toList();
 
     var totalRecords = mediatedRequestsRepository.count();
@@ -150,99 +121,6 @@ public class MediatedRequestsServiceImpl implements MediatedRequestsService {
     return mediatedRequestEntity;
   }
 
-  private MediatedRequest extendMediatedRequestDto(MediatedRequest request) {
-    log.info("extendMediatedRequestDto:: fetching item");
-    Item item = itemClient.get(request.getItemId());
-    log.info("extendMediatedRequestDto:: fetching instance");
-    Instance instance = instanceClient.get(request.getInstanceId());
-    log.info("extendMediatedRequestDto:: fetching requester");
-    User requester = userClient.get(request.getRequesterId());
-    log.info("extendMediatedRequestDto:: fetching service point");
-    ServicePoint pickupServicePoint = servicePointClient.get(request.getPickupServicePointId());
-    log.info("extendMediatedRequestDto:: fetching patron group");
-    UserGroup patronGroup = userGroupClient.get(requester.getPatronGroup());
-    log.info("extendMediatedRequestDto:: fetching location");
-    Location location = locationClient.get(item.getEffectiveLocationId());
-    log.info("extendMediatedRequestDto:: fetching library");
-    Library library = locationUnitClient.getLibrary(location.getLibraryId());
 
-    if (request.getProxyUserId() != null) {
-      log.info("extendMediatedRequestDto:: fetching proxy user");
-      User proxy = userClient.get(request.getProxyUserId());
-      log.info("extendMediatedRequestDto:: fetching proxy user patron group");
-      UserGroup proxyGroup = userGroupClient.get(proxy.getPatronGroup());
 
-      request.getProxy()
-        .barcode(proxy.getBarcode())
-        .firstName(proxy.getPersonal().getFirstName())
-        .middleName(proxy.getPersonal().getMiddleName())
-        .lastName(proxy.getPersonal().getLastName())
-        .patronGroupId(proxy.getPatronGroup())
-        .patronGroup(new MediatedRequestProxyPatronGroup()
-          .id(proxyGroup.getId())
-          .group(proxyGroup.getGroup())
-          .desc(proxyGroup.getDesc()));
-    }
-
-    List<MediatedRequestInstanceContributorNamesInner> contributors = instance.getContributors()
-      .stream()
-      .map(c -> new MediatedRequestInstanceContributorNamesInner().name(c.getName()))
-      .toList();
-
-    List<MediatedRequestInstancePublicationInner> publications = instance.getPublication()
-      .stream()
-      .map(publication -> new MediatedRequestInstancePublicationInner()
-        .publisher(publication.getPublisher())
-        .place(publication.getPlace())
-        .dateOfPublication(publication.getDateOfPublication())
-        .role(publication.getRole()))
-      .toList();
-
-    request.getItem()
-      .location(new MediatedRequestItemLocation()
-        .name(location.getName())
-        .libraryName(library.getName())
-        .code(location.getCode()))
-      .enumeration(item.getEnumeration())
-      .volume(item.getVolume())
-      .chronology(item.getChronology())
-      .displaySummary(item.getDisplaySummary())
-      .status(item.getStatus().getName().getValue())
-      .callNumber(item.getEffectiveCallNumberComponents().getCallNumber())
-      .callNumberComponents(new MediatedRequestItemCallNumberComponents()
-        .callNumber(item.getEffectiveCallNumberComponents().getCallNumber())
-        .prefix(item.getEffectiveCallNumberComponents().getPrefix())
-        .suffix(item.getEffectiveCallNumberComponents().getSuffix()))
-      .copyNumber(item.getCopyNumber());
-
-    request.getInstance()
-      .contributorNames(contributors)
-      .publication(publications)
-      .editions(new ArrayList<>(instance.getEditions()));
-
-    request.getRequester()
-      .barcode(requester.getBarcode())
-      .firstName(requester.getPersonal().getFirstName())
-      .middleName(requester.getPersonal().getMiddleName())
-      .lastName(requester.getPersonal().getLastName())
-      .patronGroupId(requester.getPatronGroup())
-      .patronGroup(new MediatedRequestRequesterPatronGroup()
-        .id(patronGroup.getId())
-        .group(patronGroup.getGroup())
-        .desc(patronGroup.getDesc()));
-
-    request.setPickupServicePoint(new MediatedRequestPickupServicePoint()
-      .name(pickupServicePoint.getName())
-      .code(pickupServicePoint.getCode())
-      .discoveryDisplayName(pickupServicePoint.getDiscoveryDisplayName())
-      .description(pickupServicePoint.getDescription())
-      .shelvingLagTime(pickupServicePoint.getShelvingLagTime())
-      .pickupLocation(pickupServicePoint.getPickupLocation()));
-
-    request.getMetadata()
-      .createdByUserId(UUID.randomUUID().toString())
-      .updatedByUserId(UUID.randomUUID().toString());
-
-    return request;
-  }
 }
