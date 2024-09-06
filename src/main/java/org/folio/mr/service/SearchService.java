121cc8604a974b8392fd0c5739b7bd58487a3c89
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-09-06 15:39:31 +0300

    MODREQMED-18 Remove integration with mod-search

--- src/main/java/org/folio/mr/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package org.folio.mr.service;
-
-import java.util.Optional;
-
-import org.folio.mr.domain.dto.SearchInstance;
-
-public interface SearchService {
-  Optional<SearchInstance> findInstance(String instanceId);
-}
