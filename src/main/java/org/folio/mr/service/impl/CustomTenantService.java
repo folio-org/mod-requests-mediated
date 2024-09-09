10d25e67b5e22eceb5c4cac6fabad626f3238576
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-09-09 15:01:40 +0300

    MODREQMED-18 Fix system user setup

--- src/main/java/org/folio/mr/service/impl/CustomTenantService.java
+++ src/main/java/org/folio/mr/service/impl/CustomTenantService.java
@@ -5,6 +5,7 @@ import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.PrepareSystemUserService;
 import org.folio.spring.service.TenantService;
 import org.folio.tenant.domain.dto.TenantAttributes;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
+@Primary
 public class CustomTenantService extends TenantService {
 
   private final PrepareSystemUserService systemUserService;
