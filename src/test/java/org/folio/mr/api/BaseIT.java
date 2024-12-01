87f071f190c446ad06d203b3fcfdb1b2afb16884
MagzhanArtykov <magzhan_artykov@epam.com>
2024-12-02 00:37:11 +0500

    MODREQMED-27 Update status of Mediated request when confirmed circulation Request is Closed as Cancelled

--- src/test/java/org/folio/mr/api/BaseIT.java
+++ src/test/java/org/folio/mr/api/BaseIT.java
@@ -2,39 +2,58 @@ package org.folio.mr.api;
 
 import static java.util.stream.Collectors.toMap;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.util.TestSocketUtils.findAvailableTcpPort;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-import org.folio.mr.util.DbInitializer;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.folio.mr.util.TestUtils;
 import org.folio.spring.FolioModuleMetadata;
 import org.folio.spring.integration.XOkapiHeaders;
 import org.folio.spring.scope.FolioExecutionContextSetter;
 import org.folio.tenant.domain.dto.TenantAttributes;
 import org.folio.test.extensions.EnablePostgres;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.springframework.test.util.TestSocketUtils;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -46,28 +65,88 @@ import lombok.SneakyThrows;
 
 @EnablePostgres
 @ActiveProfiles("test")
-@ContextConfiguration(initializers = {DbInitializer.class})
+@ContextConfiguration(initializers = BaseIT.DockerPostgresDataSourceInitializer.class)
 @EnableAutoConfiguration(exclude = {FlywayAutoConfiguration.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = {
+  "spring.kafka.consumer.auto-offset-reset=earliest"
+})
 @AutoConfigureMockMvc
+@Testcontainers
 public class BaseIT {
   protected static final String TOKEN = "test_token";
   protected static final String TENANT_ID_CONSORTIUM = "consortium";
   protected static final String TENANT_ID_COLLEGE = "college";
   protected static final String USER_ID = randomId();
   protected static final String HEADER_TENANT = "x-okapi-tenant";
+  private static final String FOLIO_ENVIRONMENT = "folio";
+
+  private static final int WIRE_MOCK_PORT = TestSocketUtils.findAvailableTcpPort();
+
+  protected static WireMockServer wireMockServer = new WireMockServer(WIRE_MOCK_PORT);
 
   @Autowired
   private WebTestClient webClient;
+
   @Autowired
   protected MockMvc mockMvc;
+
   @Autowired
   private FolioModuleMetadata moduleMetadata;
+  protected static AdminClient kafkaAdminClient;
+  @Autowired
+  protected KafkaTemplate<String, String> kafkaTemplate;
 
-  protected static WireMockServer wireMockServer = new WireMockServer(findAvailableTcpPort());
-  static {
-    wireMockServer.start();
+  @Container
+  private static final PostgreSQLContainer<?> postgresDBContainer =
+    new PostgreSQLContainer<>("postgres:12-alpine");
+  @Container
+  private static final KafkaContainer kafka = new KafkaContainer(
+    DockerImageName.parse("confluentinc/cp-kafka:7.5.3"));
+
+  @DynamicPropertySource
+  static void overrideProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    registry.add("folio.okapi-url", wireMockServer::baseUrl);
   }
+
+  @BeforeEach
+  void beforeEachTest() {
+    doPost("/_/tenant", asJsonString(new TenantAttributes().moduleTo("mod-requests-mediated")))
+      .expectStatus().isNoContent();
+
+    contextSetter = initFolioContext();
+    wireMockServer.resetAll();
+  }
+
+  @AfterEach
+  public void afterEachTest() {
+    contextSetter.close();
+  }
+
+  @BeforeAll
+  static void setUp() {
+    wireMockServer = new WireMockServer(WIRE_MOCK_PORT);
+    wireMockServer.start();
+
+    kafkaAdminClient = KafkaAdminClient.create(Map.of(
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers()));
+    createKafkaTopics(KAFKA_TOPICS);
+  }
+
+  @AfterAll
+  static void tearDown() {
+    kafkaAdminClient.close();
+    wireMockServer.stop();
+  }
+
+  protected static final String REQUEST_KAFKA_TOPIC_NAME =
+    buildTopicName("circulation", "request");
+
+  private static final String[] KAFKA_TOPICS = {
+    REQUEST_KAFKA_TOPIC_NAME
+  };
 
   private FolioExecutionContextSetter contextSetter;
 
@@ -76,25 +155,15 @@ public class BaseIT {
     .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
-  @DynamicPropertySource
-  static void overrideProperties(DynamicPropertyRegistry registry) {
-    registry.add("folio.okapi-url", wireMockServer::baseUrl);
-  }
+  @SneakyThrows
+  private static void createKafkaTopics(String... topicNames) {
+    List<NewTopic> topics = Arrays.stream(topicNames)
+      .map(topic -> new NewTopic(topic, 1, (short) 1))
+      .toList();
 
-  @BeforeAll
-  static void beforeAll(@Autowired MockMvc mockMvc) {
-    setUpTenant(mockMvc);
-  }
-
-  @BeforeEach
-  void beforeEachTest() {
-    contextSetter = initFolioContext();
-    wireMockServer.resetAll();
-  }
-
-  @AfterEach
-  public void afterEachTest() {
-    contextSetter.close();
+    kafkaAdminClient.createTopics(topics)
+      .all()
+      .get(10, TimeUnit.SECONDS);
   }
 
   protected FolioExecutionContextSetter initFolioContext() {
@@ -111,6 +180,16 @@ public class BaseIT {
       .content(asJsonString(new TenantAttributes().moduleTo("mod-requests-mediated")))
       .headers(defaultHeaders())
       .contentType(APPLICATION_JSON)).andExpect(status().isNoContent());
+  }
+
+  public static class DockerPostgresDataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+      TestPropertySourceUtils.addInlinedPropertiesToEnvironment(applicationContext,
+        "spring.datasource.url=" + postgresDBContainer.getJdbcUrl(),
+        "spring.datasource.username=" + postgresDBContainer.getUsername(),
+        "spring.datasource.password=" + postgresDBContainer.getPassword());
+    }
   }
 
   public static HttpHeaders defaultHeaders() {
@@ -159,6 +238,14 @@ public class BaseIT {
       .cookie("folioAccessToken", token)
       .body(BodyInserters.fromValue(payload))
       .exchange();
+  }
+
+  protected static String buildTopicName(String module, String objectType) {
+    return buildTopicName(FOLIO_ENVIRONMENT, TENANT_ID_CONSORTIUM, module, objectType);
+  }
+
+  private static String buildTopicName(String env, String tenant, String module, String objectType) {
+    return String.format("%s.%s.%s.%s", env, tenant, module, objectType);
   }
 
   protected static String randomId() {
