63e26ff37d3b72a0498e7ba5e6bc868f1c373af2
OleksandrVidinieiev <56632770+OleksandrVidinieiev@users.noreply.github.com>
2025-05-20 18:12:30 +0200

    MODREQMED-122: Check-out API (#91)
    * MODREQMED-122 Check-out API
    
    * MODREQMED-122 Fix item search
    
    * MODREQMED-122 Add prefix to cloned loan policy name
    
    * MODREQMED-122 Handle collection of fake patron links
    
    * MODREQMED-122 Find request for check-out
    
    * MODREQMED-122 Allow regular check-outs
    
    * MODREQMED-122 Clean up
    
    * MODREQMED-122 Clean up
    
    * MODREQMED-122 Clean up
    
    * MODREQMED-122 Fetch request from circulation-storage
    
    * MODREQMED-122 API tests
    
    * MODREQMED-122 API tests
    
    * MODREQMED-122 Error forwarding test
    
    * MODREQMED-122 Fix code smells
    
    * MODREQMED-122 Remove redundant changes
    
    * MODREQMED-122 Remove comments in tests

--- src/test/java/org/folio/mr/util/MockHelper.java
+++ src/test/java/org/folio/mr/util/MockHelper.java
@@ -1,18 +1,37 @@
 package org.folio.mr.util;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.folio.spring.integration.XOkapiHeaders.TENANT;
 import static org.folio.test.TestUtils.asJsonString;
 
+import org.apache.http.HttpStatus;
+import org.folio.mr.domain.dto.BatchIds;
+import org.folio.mr.domain.dto.CheckOutDryRunRequest;
+import org.folio.mr.domain.dto.CheckOutDryRunResponse;
+import org.folio.mr.domain.dto.CheckOutRequest;
+import org.folio.mr.domain.dto.CheckOutResponse;
 import org.folio.mr.domain.dto.ConsortiumItem;
-import org.folio.spring.integration.XOkapiHeaders;
+import org.folio.mr.domain.dto.ConsortiumItems;
+import org.folio.mr.domain.dto.LoanPolicy;
+import org.folio.mr.domain.dto.Request;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 
 public class MockHelper {
   private static final String ITEM_SEARCH_URL = "/search/consortium/item";
+  private static final String ITEM_BATCH_SEARCH_URL = "/search/consortium/batch/items";
+  private static final String CIRCULATION_STORAGE_REQUESTS_URL = "/request-storage/requests";
+  private static final String CIRCULATION_CHECK_OUT_URL = "/circulation/check-out-by-barcode";
+  private static final String CIRCULATION_CHECK_OUT_DRY_RUN_URL =
+    "/circulation/check-out-by-barcode-dry-run";
+  private static final String LOAN_POLICIES_URL = "/loan-policy-storage/loan-policies";
 
   private final WireMockServer wireMockServer;
 
@@ -22,8 +41,52 @@ public class MockHelper {
 
   public void mockItemSearch(String tenantId, String itemId, ConsortiumItem mockItem) {
     wireMockServer.stubFor(get(urlPathEqualTo(ITEM_SEARCH_URL + "/" + itemId))
-      .withHeader(XOkapiHeaders.TENANT, equalTo(tenantId))
+      .withHeader(TENANT, equalTo(tenantId))
       .willReturn(okJson(asJsonString(mockItem))));
+  }
+
+  public void mockItemBatchSearch(String tenantId, BatchIds batchIds, ConsortiumItems consortiumItems) {
+    wireMockServer.stubFor(post(urlPathEqualTo(ITEM_BATCH_SEARCH_URL))
+      .withRequestBody(equalTo(asJsonString(batchIds)))
+      .withHeader(TENANT, equalTo(tenantId))
+      .willReturn(okJson(asJsonString(consortiumItems))));
+  }
+
+  public void mockGetRequest(Request request, String tenantId) {
+    wireMockServer.stubFor(get(urlPathEqualTo(CIRCULATION_STORAGE_REQUESTS_URL + "/" + request.getId()))
+      .withHeader(TENANT, equalTo(tenantId))
+      .willReturn(okJson(asJsonString(request))));
+  }
+
+  public void mockCirculationCheckOut(CheckOutRequest request, CheckOutResponse response,
+    String tenantId) {
+
+    wireMockServer.stubFor(post(urlEqualTo(CIRCULATION_CHECK_OUT_URL))
+      .withRequestBody(equalTo(asJsonString(request)))
+      .withHeader(TENANT, equalTo(tenantId))
+      .willReturn(okJson(asJsonString(response))));
+  }
+
+  public void mockCirculationCheckOutDryRun(CheckOutDryRunRequest request,
+    CheckOutDryRunResponse response, String tenantId) {
+
+    wireMockServer.stubFor(post(urlEqualTo(CIRCULATION_CHECK_OUT_DRY_RUN_URL))
+      .withRequestBody(equalToJson(asJsonString(request)))
+      .withHeader(TENANT, equalTo(tenantId))
+      .willReturn(okJson(asJsonString(response))));
+  }
+
+  public void mockGetLoanPolicy(LoanPolicy loanPolicy, String tenantId) {
+    wireMockServer.stubFor(get(urlEqualTo(LOAN_POLICIES_URL + "/" + loanPolicy.getId()))
+      .withHeader(TENANT, equalTo(tenantId))
+      .willReturn(okJson(asJsonString(loanPolicy))));
+  }
+
+  public void mockPostLoanPolicy(LoanPolicy loanPolicy, String tenantId) {
+    wireMockServer.stubFor(post(urlEqualTo(LOAN_POLICIES_URL))
+      .withRequestBody(equalToJson(asJsonString(loanPolicy)))
+      .withHeader(TENANT, equalTo(tenantId))
+      .willReturn(jsonResponse(asJsonString(loanPolicy), HttpStatus.SC_CREATED)));
   }
 
 }
