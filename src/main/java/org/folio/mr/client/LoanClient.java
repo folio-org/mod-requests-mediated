46af986255c077605be04b26522efbc533ff2c4b
Alexander Kurash <Oleksandr_Kurash@epam.com>
2025-07-11 19:49:06 +0300

    MODREQMED-134 Declare lost API for mediated requests (#104)
    * MODREQMED-134 Initial implementation
    
    * MODREQMED-134 Rename client beans
    
    * MODREQMED-134 Change parameter types
    
    * MODREQMED-134 Add service point ID
    
    * MODREQMED-134 Add tests
    
    * MODREQMED-134 Add system user permissions
    
    * MODREQMED-134 Increase test coverage
    
    * MODREQMED-134 Fix quality gate issue
    
    * MODREQMED-134 Add ecs-loans dependency
    
    * MODREQMED-134 Code review changes
    
    * MODREQMED-134 Change formatting
    
    (cherry picked from commit 81b40f18d7c0497c64d3133b18f47c04942c7726)

---
+++ src/main/java/org/folio/mr/client/LoanClient.java
@@ -0,0 +1,18 @@
+package org.folio.mr.client;
+
+import java.util.Optional;
+
+import org.folio.mr.domain.dto.Loan;
+import org.folio.spring.config.FeignClientConfiguration;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "loan-storage", url = "loan-storage/loans",
+  configuration = FeignClientConfiguration.class, dismiss404 = true)
+public interface LoanClient {
+
+  @GetMapping("/{loanId}")
+  Optional<Loan> getLoanById(@PathVariable("loanId") String loanId);
+
+}
--- src/main/java/org/folio/mr/controller/MediatedRequestLoansController.java
+++ src/main/java/org/folio/mr/controller/MediatedRequestLoansController.java
@@ -1,10 +1,14 @@
 package org.folio.mr.controller;
 
+import java.util.UUID;
+
 import org.folio.mr.domain.dto.CheckOutRequest;
 import org.folio.mr.domain.dto.CheckOutResponse;
+import org.folio.mr.domain.dto.DeclareLostCirculationRequest;
 import org.folio.mr.exception.HttpFailureFeignException;
 import org.folio.mr.rest.resource.MediatedRequestsLoansApi;
 import org.folio.mr.service.CheckOutService;
+import org.folio.mr.service.DeclareLostService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +22,19 @@ import lombok.extern.log4j.Log4j2;
 public class MediatedRequestLoansController implements MediatedRequestsLoansApi {
 
   private final CheckOutService checkOutService;
+  private final DeclareLostService declareLostService;
 
   @Override
   public ResponseEntity<CheckOutResponse> checkOutByBarcode(CheckOutRequest request) {
     return ResponseEntity.ok(checkOutService.checkOut(request));
+  }
+
+  @Override
+  public ResponseEntity<Void> declareItemLost(UUID loanId,
+    DeclareLostCirculationRequest declareLostRequest) {
+
+    declareLostService.declareLost(loanId, declareLostRequest);
+    return ResponseEntity.noContent().build();
   }
 
   @ExceptionHandler(HttpFailureFeignException.class)
