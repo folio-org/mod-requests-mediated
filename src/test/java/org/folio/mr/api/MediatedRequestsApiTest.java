c22af98090d5ac0635da92714f881ad859bec796
Roman_Barannyk <roman_barannyk@epam.com>
2026-01-28 14:31:44 +0200

    MODREQMED-199 revert redundant changes

--- src/test/java/org/folio/mr/api/MediatedRequestsApiTest.java
+++ src/test/java/org/folio/mr/api/MediatedRequestsApiTest.java
@@ -734,6 +734,11 @@ class MediatedRequestsApiTest extends BaseIT {
     "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu " +
     "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
     "officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur " +
+    "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
+    "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+    "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu " +
+    "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+    "officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur " +
     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut " +
     "enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
     "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
