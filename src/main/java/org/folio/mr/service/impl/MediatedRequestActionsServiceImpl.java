dd3d13491fd9d5f1f9a45493d04590e8ecb7e1b2
alexanderkurash <alexander.kurash@gmail.com>
2026-05-07 12:37:57 +0300

    MODREQMED-223 Revert findItem method

--- src/main/java/org/folio/mr/service/impl/MediatedRequestActionsServiceImpl.java
+++ src/main/java/org/folio/mr/service/impl/MediatedRequestActionsServiceImpl.java
@@ -298,12 +298,10 @@ public class MediatedRequestActionsServiceImpl implements MediatedRequestActions
   }
 
   private void findItem(MediatedRequestContext context) {
-    String itemId = context.getRequest().getItemId();
-    var searchItem = searchService.searchItem(itemId)
-      .orElseThrow(() -> ExceptionFactory.notFound(format("Item %s not found", itemId)));
-
-    context.setLendingTenantId(searchItem.getTenantId());
-    fetchItem(context);
+    searchService.searchItem(context.getRequest().getItemId())
+      .map(ConsortiumItem::getTenantId)
+      .map(context::setLendingTenantId)
+      .ifPresent(this::fetchItem);
   }
 
   private void findRequester(MediatedRequestContext context) {
